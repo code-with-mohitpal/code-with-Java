87f641fd1a08b87d67747d12e6d09973bbd99e05
Mohit pal <pal001768@gmail.com>
2026-01-12 21:46:28 +0530

    variable

---
+++ Variable&DataTypes/VariablesAndDataTypes.java
@@ -0,0 +1,25 @@
+public class VariablesAndDataTypes {
+    public static void main(String[] args) {
+
+        int age = 20;
+        float height = 5.9f;
+        double weight = 65.75;
+        char grade = 'A';
+        boolean isStudent = true;
+        byte level = 3;
+        short year = 2025;
+        long population = 1400000000L;
+
+        String name = "Mohit";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Weight: " + weight);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+        System.out.println("Level: " + level);
+        System.out.println("Year: " + year);
+        System.out.println("Population: " + population);
+    }
+}
