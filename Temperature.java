50a9cfc7533c0c1ac2cfc9582c727b334c156b13
Mohit pal <pal001768@gmail.com>
2026-01-13 23:11:25 +0530

    temperatre

---
+++ Temperature.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Temperature {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        float celsius = sc.nextFloat();
+        float fahrenheit = (celsius * 9 / 5) + 32;
+
+        System.out.println("Fahrenheit = " + fahrenheit);
+    }
+}
+
