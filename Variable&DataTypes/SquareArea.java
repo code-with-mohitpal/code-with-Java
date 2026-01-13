e7bbef0e2b6f8011fd6051527243929796076952
Mohit pal <pal001768@gmail.com>
2026-01-13 23:01:12 +0530

    Area of a Square

---
+++ Variable&DataTypes/SquareArea.java
@@ -0,0 +1,14 @@
+# area of square
+import java.util.Scanner;
+
+public class SquareArea {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int side = sc.nextInt();
+        int area = side * side;
+
+        System.out.println("Area = " + area);
+    }
+}
+
