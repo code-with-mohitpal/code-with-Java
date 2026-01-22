75531c87d407c3b602616aa1ccb86afe74c706f6
Mohit pal <pal001768@gmail.com>
2026-01-22 22:28:49 +0530

    change

---
+++ function/MathFunctions.java
@@ -0,0 +1,12 @@
+public class MathFunctions {
+
+    public static void main(String[] args) {
+
+        System.out.println("Min: " + Math.min(10, 20));
+        System.out.println("Max: " + Math.max(10, 20));
+        System.out.println("Square Root: " + Math.sqrt(25));
+        System.out.println("Power: " + Math.pow(2, 3));
+        System.out.println("Absolute: " + Math.abs(-15));
+    }
+}
+
