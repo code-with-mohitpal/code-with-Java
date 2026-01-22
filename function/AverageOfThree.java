9003dc9cefeff35c7166e04a68c3a8bf33d5c01d
Mohit pal <pal001768@gmail.com>
2026-01-22 22:25:00 +0530

    change

---
+++ function/AverageOfThree.java
@@ -0,0 +1,10 @@
+public class AverageOfThree {
+    public static double average(int a, int b, int c) {
+        return (a + b + c) / 3.0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Average = " + average(10, 20, 30));
+    }
+}
+
