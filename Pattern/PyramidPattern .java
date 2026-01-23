de1d8e6eab808a72dc4e62bf651546aae80b163a
Mohit pal <pal001768@gmail.com>
2026-01-23 23:46:24 +0530

    change

---
+++ Pattern/PyramidPattern .java
@@ -0,0 +1,15 @@
+public class PyramidPattern {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 1; i <= n; i++) {
+            for (int space = i; space < n; space++) {
+                System.out.print(" ");
+            }
+            for (int star = 1; star <= (2 * i - 1); star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
