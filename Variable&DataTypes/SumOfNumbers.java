1cec13f59ab7a148283087f0bd37da5e9ea95e91
Mohit pal <pal001768@gmail.com>
2026-01-24 21:58:59 +0530

    change

---
+++ Variable&DataTypes/SumOfNumbers.java
@@ -0,0 +1,15 @@
+public class SumOfNumbers {
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 4;
+        int c = 6;
+        int d = 8;
+        int e = 10;
+
+        int sum = a + b + c + d + e;
+
+        System.out.println("Sum of 5 numbers = " + sum);
+    }
+}
+
