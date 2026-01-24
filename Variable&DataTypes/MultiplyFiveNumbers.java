f6caa74c5eda8eabc8501d8383e7c903b672b276
Mohit pal <pal001768@gmail.com>
2026-01-24 22:01:13 +0530

    change

---
+++ Variable&DataTypes/MultiplyFiveNumbers.java
@@ -0,0 +1,14 @@
+public class MultiplyFiveNumbers {
+    public static void main(String[] args) {
+
+        int a = 2;
+        int b = 3;
+        int c = 4;
+        int d = 5;
+        int e = 6;
+
+        int product = a * b * c * d * e;
+
+        System.out.println("Multiplication of 5 numbers = " + product);
+    }
+}
