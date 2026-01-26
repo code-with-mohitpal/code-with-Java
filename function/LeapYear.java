a85c069ca3b2de483ff0191ce88dfc7760550a9a
Mohit pal <pal001768@gmail.com>
2026-01-26 22:33:27 +0530

    change

---
+++ function/LeapYear.java
@@ -0,0 +1,11 @@
+public class LeapYear {
+
+    static boolean isLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isLeapYear(2024));
+    }
+}
+
