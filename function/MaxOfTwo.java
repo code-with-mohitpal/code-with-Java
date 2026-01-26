f276f750320b685d35e1d802b1d933f7b406f53f
Mohit pal <pal001768@gmail.com>
2026-01-26 22:30:52 +0530

    change

---
+++ function/MaxOfTwo.java
@@ -0,0 +1,11 @@
+public class MaxOfTwo {
+
+    static int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Max = " + max(10, 20));
+    }
+}
+
