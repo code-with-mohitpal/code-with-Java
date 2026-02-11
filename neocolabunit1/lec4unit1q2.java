352d82183fd534a78a811a0d4f921588607e28a4
Mohit pal <pal001768@gmail.com>
2026-02-11 09:08:41 +0530

    Create lec4unit1q2.java

---
+++ neocolabunit1/lec4unit1q2.java
@@ -0,0 +1,54 @@
+// Single File Programming Question
+// Problem Statement
+
+
+
+// An online store is simplifying the payment process for its customers. After a user checks out their items and the total expenditure is calculated, the system should generate a payment request. The request should prompt the user to make the payment via Google Pay, specifying the exact amount. Once the payment request is made, the system should display a thank-you message.
+
+
+
+// Write a program that takes the total expenditure as input and prints a payment request message along with a thank-you note.
+
+// Input format :
+// The input consists of a double value, representing the total expenditure.
+
+// Output format :
+// The first line of output prints "Please Gpay Rs." followed by a double value representing the total expenditure value.
+
+// The second line of output prints "Thanks, Visit us again!".
+
+
+
+// Refer to the sample output for the exact text and format.
+
+// Code constraints :
+// The given testcases fall under the following constraints:
+
+// 100.00 ≤ expenditure ≤ 5000.00
+
+// Sample test cases :
+// Input 1 :
+// 350.25
+// Output 1 :
+// Please Gpay Rs.350.25
+// Thanks, Visit us again!
+// Input 2 :
+// 1200.59
+// Output 2 :
+// Please Gpay Rs.1200.59
+// Thanks, Visit us again!
+// Note :
+
+
+
+
+import java.util.Scanner;
+public class Main {    
+    public static void main(String[] args) {       
+        Scanner sc = new Scanner(System.in);   
+        double expenditure = sc.nextDouble();      
+        System.out.println("Please Gpay Rs." + expenditure);     
+        System.out.println("Thanks, Visit us again!");  
+        }
+    
+}
