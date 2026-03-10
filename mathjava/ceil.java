0cd684d35ae91d69d764963c03d2aeba5ab06ad0
Mohit pal <pal001768@gmail.com>
2026-03-10 21:34:47 +0530

    change

---
+++ mathjava/ceil.java
@@ -0,0 +1,9 @@
+public class ceil {
+    public static void main(String[] args) {
+        double num = 5.7;
+
+        System.out.println(Math.round(num)); // nearest integer
+        System.out.println(Math.floor(num)); // lower integer
+        System.out.println(Math.ceil(num));  // higher integer
+    }
+}
