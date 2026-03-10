ec4d420fba45c7a24ff8054859457d7b0460fbbb
Mohit pal <pal001768@gmail.com>
2026-03-10 21:36:10 +0530

    change

---
+++ mathjava/min.java
@@ -0,0 +1,8 @@
+public class min {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        System.out.println(Math.max(a, b));
+        System.out.println(Math.min(a, b));
+    }
+}
