3c077f1df26ab6b2d0cace57c5b7a9c1f842bcda
Mohit pal <pal001768@gmail.com>
2026-03-12 09:53:03 +0530

    change

--- CLasssroom/Abstract.java
+++ CLasssroom/Abstract.java
@@ -1,3 +1,29 @@
-public class Abstract {
+abstract class File {
+    public File{
+
+    }
+    void reading(){
+        System.out.println("Reading file.....");
+    }
+    abstract void writing(){
+
+    }
+
+
     
+}
+class Data extends File {
+    @Override
+    public void writing(){
+        System.out.println("writing data in file....");
+    }
+
+
+}
+class Abstraction{
+    public static void main(String[] args) {
+        Data d1=new Data();
+        d1.writing();
+        d1.reading();
+    }
 }
