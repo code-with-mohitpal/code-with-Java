5772bbdf2e7cb5091b782d0b643747c82feae813
Mohit pal <pal001768@gmail.com>
2026-04-09 18:53:12 +0530

    Create Method Overriding.java

---
+++ CLasssroom/Method Overriding.java
@@ -0,0 +1,16 @@
+class Animal {
+    void sound() {
+        System.out.println("Animal sound");
+    }
+}
+
+class Cat extends Animal {
+    void sound() {
+        System.out.println("Meow");
+    }
+
+    public static void main(String[] args) {
+        Animal a = new Cat();
+        a.sound();
+    }
+}
