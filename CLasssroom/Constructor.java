24947c418fc6e423ec9ac016f72252e2a9d6d01a
Mohit pal <pal001768@gmail.com>
2026-04-09 18:51:11 +0530

    Create Constructor.java

---
+++ CLasssroom/Constructor.java
@@ -0,0 +1,18 @@
+class Student {
+    String name;
+    int age;
+
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void display() {
+        System.out.println(name + " " + age);
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Rahul", 21);
+        s1.display();
+    }
+}
