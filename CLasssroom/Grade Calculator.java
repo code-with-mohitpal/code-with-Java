b336262f4d4d1047037409905932c63f96134662
Mohit pal <pal001768@gmail.com>
2026-04-11 19:22:55 +0530

    Create Grade Calculator.java

---
+++ CLasssroom/Grade Calculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Grade {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int marks = sc.nextInt();
+
+        if (marks >= 90) {
+            System.out.println("A");
+        } else if (marks >= 75) {
+            System.out.println("B");
+        } else if (marks >= 50) {
+            System.out.println("C");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
