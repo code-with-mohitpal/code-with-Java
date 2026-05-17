e454a463c379a673141c0ffb4f8d0d9ed40d2285
Mohit pal <pal001768@gmail.com>
2026-05-17 17:51:31 +0530

    Create Enum Example.java

---
+++ CLasssroom/Enum Example.java
@@ -0,0 +1,11 @@
+enum Day {
+    MONDAY, TUESDAY, WEDNESDAY
+}
+
+public class EnumExample {
+    public static void main(String[] args) {
+        Day today = Day.MONDAY;
+
+        System.out.println("Today is " + today);
+    }
+}
