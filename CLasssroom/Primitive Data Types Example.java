e049c0dbc43ca3d8af713028a1af60007b213522
Mohit pal <pal001768@gmail.com>
2026-05-18 22:09:08 +0530

    Add example of primitive data types in Java

---
+++ CLasssroom/Primitive Data Types Example.java
@@ -0,0 +1,14 @@
+class DataTypes {
+    public static void main(String[] args) {
+
+        int age = 20;
+        float marks = 85.5f;
+        char grade = 'A';
+        boolean result = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+        System.out.println("Grade: " + grade);
+        System.out.println("Result: " + result);
+    }
+}
