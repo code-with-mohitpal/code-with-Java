3286f673cd7a78d9eec0c2ab2206f602f3e46455
Mohit pal <pal001768@gmail.com>
2026-05-18 22:11:04 +0530

    Add WrapperExample class for demonstrating boxing

---
+++ CLasssroom/Wrapper Class Example.java
@@ -0,0 +1,11 @@
+class WrapperExample {
+    public static void main(String[] args) {
+
+        int num = 10;
+
+        Integer obj = Integer.valueOf(num);
+
+        System.out.println("Primitive value: " + num);
+        System.out.println("Wrapper Object: " + obj);
+    }
+}
