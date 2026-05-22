cdf87c20818a7c25fd70be08514bb6bde318fad2
Mohit pal <pal001768@gmail.com>
2026-05-22 22:38:32 +0530

    Create Current Time Example.java

---
+++ CLasssroom/Current Time Example.java
@@ -0,0 +1,10 @@
+import java.time.LocalTime;
+
+class CurrentTime {
+    public static void main(String[] args) {
+
+        LocalTime time = LocalTime.now();
+
+        System.out.println("Current Time : " + time);
+    }
+}
