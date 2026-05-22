79bfa43eeb0583127c24f8e53c25b0976b9bb707
Mohit pal <pal001768@gmail.com>
2026-05-22 22:39:35 +0530

    Add Current Date Example Java program

---
+++ CLasssroom/Current Date Example.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+
+class CurrentDate {
+    public static void main(String[] args) {
+
+        LocalDate today = LocalDate.now();
+
+        System.out.println("Today's Date : " + today);
+    }
+}
