8f2782f6185dde281048da49fc90e782829c92bb
Mohit pal <pal001768@gmail.com>
2026-05-29 23:04:25 +0530

    Create ArrayList Size Example.java

---
+++ CLasssroom/ArrayList Size Example.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.println("Size = " + list.size());
+    }
+}
