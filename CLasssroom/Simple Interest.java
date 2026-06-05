2f179b39ec0746f561bcd0d31dc1cdf18b83c328
Mohit pal <pal001768@gmail.com>
2026-06-05 22:07:39 +0530

    Create Simple Interest.java

---
+++ CLasssroom/Simple Interest.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Q9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Principal: ");
+        float P = sc.nextFloat();
+
+        System.out.print("Enter Rate: ");
+        float R = sc.nextFloat();
+
+        System.out.print("Enter Time: ");
+        float T = sc.nextFloat();
+
+        float SI = (P * R * T) / 100;
+
+        System.out.println("Simple Interest = " + SI);
+    }
+}
