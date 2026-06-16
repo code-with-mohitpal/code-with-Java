ed9590782223c502a44e9b3c13bde224c1ee3cc6
Mohit pal <pal001768@gmail.com>
2026-06-16 13:58:22 +0530

    change

---
+++ Array1/array.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class array{
+    public static void main(String[] args) {
+        int numbers[]=new int[50];
+        int Numbers[]={1,2,3,4,5,5};
+        System.out.println(Arrays.toString(Numbers));
+    }
+}
